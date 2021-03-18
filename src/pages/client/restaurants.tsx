3c048cb4aadacf64ee39efc66466211f01ff4f53
Julee Kim <summerbreeze0323@gmail.com>
2021-03-18 15:29:03 +0900

    Restaurants Pagination

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,5 +1,6 @@
 import { gql, useQuery } from '@apollo/client';
-import React from 'react';
+import React, { useState } from 'react';
+import { Restaurant } from '../../components/restaurant';
 import { restaurantsPageQuery, restaurantsPageQueryVariables } from '../../__generated__/restaurantsPageQuery';
 
 const RESTAURANTS_QUERY = gql`
@@ -35,16 +36,19 @@ const RESTAURANTS_QUERY = gql`
 `;
 
 export const Restaurants = () => {
+  const [page, setPage] = useState(1);
   const { data, loading } = useQuery<
     restaurantsPageQuery,
     restaurantsPageQueryVariables
     >(RESTAURANTS_QUERY, {
       variables: {
         input: {
-        page: 1
+        page
       }
     }
   });
+  const onNextPageClick = () => setPage((current) => current + 1);
+  const onPrevPageClick = () => setPage((current) => current - 1);
 
   return (
     <div>
@@ -56,7 +60,7 @@ export const Restaurants = () => {
         />
       </form>
       {!loading && (
-        <div className="max-w-screen-2xl mx-auto mt-8">
+        <div className="max-w-screen-2xl pb-20 mx-auto mt-8">
           <div className="flex justify-around max-w-sm mx-auto">
             {data?.allCategories.categories?.map((category, index) => (
               <div className="flex flex-col items-center rounded-full cursor-pointer hover:text-gray-500" key={index}>
@@ -71,19 +75,35 @@ export const Restaurants = () => {
               </div>
             ))}
           </div>
-          <div className="grid mt-10 grid-cols-3 gap-x-5 gap-y-10">
+          <div className="grid mt-16 grid-cols-3 gap-x-5 gap-y-10">
             {data?.restaurants.results?.map((restaurant, index) => (
-              <div key={index}>
-                <div
-                  style={{ backgroundImage: `url(${restaurant.coverImg})` }}
-                  className="bg-red-500 bg-cover bg-center mb-3 py-28"
-                ></div>
-                <h3 className="text-xl font-medium">{restaurant.name}</h3>
-                <span className="border-t-2 border-gray-200">
-                  {restaurant.category?.name}
-                </span>
-              </div>
+              <Restaurant
+                key={index}
+                id={restaurant.id + ''}
+                name={restaurant.name}
+                coverImg={restaurant.coverImg}
+                categoryName={restaurant.category?.name}
+              />
             ))}
+          </div>
+          <div className="grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10">
+            {page > 1 ? (
+              <button
+                onClick={onPrevPageClick}
+                className="focus:outline-none font-medium text-2xl"
+              >&larr;</button>
+            ) : (
+              <div></div>
+            )}
+            <span>Page {page} of {data?.restaurants.totalPages}</span>
+            {page !== data?.restaurants.totalPages ? (
+              <button
+                onClick={onNextPageClick}
+                className="focus:outline-none font-medium text-2xl"
+              >&rarr;</button>
+            ) : (
+              <div></div>
+            )}
           </div>
         </div>
       )}
