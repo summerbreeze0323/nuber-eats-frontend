42a11eb4e7cdadf34a5ee3f6241b4dc2c708c5e8
Julee Kim <summerbreeze0323@gmail.com>
2021-03-18 22:09:11 +0900

    Login Tests part One

---
+++ src/components/__tests__/login.spec.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { render, RenderResult, waitFor } from "@testing-library/react";
+import { Login } from "../../pages/login";
+import { ApolloProvider } from "@apollo/client";
+import { createMockClient } from "mock-apollo-client";
+import { HelmetProvider } from "react-helmet-async";
+import { BrowserRouter as Router } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+
+describe('<Login/>', () => {
+  let renderResult: RenderResult;
+
+  beforeEach(async () => {
+    await waitFor(() => {
+      const mockedClient = createMockClient();
+
+      renderResult = render(
+        <HelmetProvider>
+          <Router>
+            <ApolloProvider client={mockedClient}>
+              <Login />
+            </ApolloProvider>
+          </Router>
+        </HelmetProvider>
+      );
+    })
+  });
+
+  it('should render OK', async () => {
+    await waitFor(() => {
+      expect(document.title).toBe('Login | Nuber Eats');
+    });
+  });
+
+  it('displays email validation errors', async () => {
+    await waitFor(async () => {
+      const { getByPlaceholderText, debug, getByRole } = renderResult;
+      const email = getByPlaceholderText(/email/i); // /email/i: 대소문자 구분 안함
+      await waitFor(() => {
+        userEvent.type(email, 'this@wont');
+      });
+      
+      let errorMessage = getByRole('alert');
+      expect(errorMessage).toHaveTextContent(/please enter a valid email/i);
+      await waitFor(() => {
+        userEvent.clear(email);
+        debug();
+      })
+    });
+  });
+});
