497548bba3468ceee47eb3ca90cfc311fea6b534
Julee Kim <summerbreeze0323@gmail.com>
2021-03-18 17:56:04 +0900

    Restaurant part Two

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -1,5 +1,6 @@
 import { gql, useQuery } from '@apollo/client';
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import { useParams } from 'react-router';
 import { RESTAURANT_FRAGMENT } from '../../fragments';
 import { restaurant, restaurantVariables } from '../../__generated__/restaurant';
@@ -30,9 +31,28 @@ export const Restaurant = () => {
       }
     }
   });
-  console.log(data)
 
   return (
-    <h1>restaurant</h1>
+    <div>
+      <Helmet>
+        <title>{data?.restaurant.restaurant?.name || ""} | Nuber Eats</title>
+      </Helmet>
+      <div
+        className=" bg-gray-800 bg-center bg-cover py-48"
+        style={{
+          backgroundImage: `url(${data?.restaurant.restaurant?.coverImg})`,
+        }}
+      >
+        <div className="bg-white w-3/12 py-8 pl-48">
+          <h4 className="text-4xl mb-3">{data?.restaurant.restaurant?.name}</h4>
+          <h5 className="text-sm font-light mb-2">
+            {data?.restaurant.restaurant?.category?.name}
+          </h5>
+          <h6 className="text-sm font-light">
+            {data?.restaurant.restaurant?.address}
+          </h6>
+        </div>
+      </div>
+    </div>
   );
 };
