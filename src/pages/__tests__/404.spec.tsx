35ac456b5c0e0fdc9f7d129dafd06bb7c6a9aad5
Julee Kim <summerbreeze0323@gmail.com>
2021-03-18 23:41:51 +0900

    login, 404 tests 폴더 이동

--- src/pages/__tests__/404.spec.tsx
+++ src/pages/__tests__/404.spec.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, waitFor } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import { NotFound } from "../../pages/404";
+import { NotFound } from "../404";
 
 describe('<NotFound/>', () => {
   it('renders OK', async () => {
