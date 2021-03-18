b5fe9fde08ffc2a26417cc2840a95a00acb12764
Julee Kim <summerbreeze0323@gmail.com>
2021-03-18 17:23:36 +0900

    (challenge)Category page

--- src/pages/client/category.tsx
+++ src/pages/client/category.tsx
@@ -1,6 +1,8 @@
 import { gql, useQuery } from '@apollo/client';
-import React from 'react';
+import React, { useState } from 'react';
+import { Helmet } from 'react-helmet-async';
 import { useParams } from 'react-router';
+import { Restaurant } from '../../components/restaurant';
 import { CATEGORY_FRAGMENT, RESTAURANT_FRAGMENT } from '../../fragments';
 import { category, categoryVariables } from '../../__generated__/category';
 
@@ -29,12 +31,13 @@ interface ICategoryParams {
 
 export const Category = () => {
   const params = useParams<ICategoryParams>();
+  const [page, setPage] = useState(1);
   const { data, loading } = useQuery<category, categoryVariables>(
     CATEGORY_QUERY,
     {
       variables: {
         input: {
-          page: 1,
+          page,
           slug: params.slug,
         },
       },
@@ -42,7 +45,50 @@ export const Category = () => {
   );
   console.log(data);
 
+  const onNextPageClick = () => setPage((current) => current + 1);
+  const onPrevPageClick = () => setPage((current) => current - 1);
+
   return (
-    <h1>category!!</h1>
+    <div>
+      <Helmet>
+        <title>{ params.slug } | Nuber Eats</title>
+      </Helmet>
+      <h2>{params.slug} Category</h2>
+      <h3>{data?.category.totalResults} Restaurants</h3>
+      {!loading && data?.category.restaurants?.length && (
+        <div className="max-w-screen-2xl pb-20 mx-auto mt-8">
+          <div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
+            {data?.category.restaurants?.map((restaurant, index) => (
+              <Restaurant
+                key={index}
+                id={restaurant.id + ''}
+                name={restaurant.name}
+                coverImg={restaurant.coverImg}
+                categoryName={restaurant.category?.name}
+              />
+            ))}
+          </div>
+          <div className="grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10">
+            {page > 1 ? (
+              <button
+                onClick={onPrevPageClick}
+                className="focus:outline-none font-medium text-2xl"
+              >&larr;</button>
+            ) : (
+              <div></div>
+            )}
+            <span>Page {page} of {data?.category.totalPages}</span>
+            {page !== data?.category.totalPages ? (
+              <button
+                onClick={onNextPageClick}
+                className="focus:outline-none font-medium text-2xl"
+              >&rarr;</button>
+            ) : (
+              <div></div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
