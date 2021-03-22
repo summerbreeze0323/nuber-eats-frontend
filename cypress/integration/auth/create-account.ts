39f600e93c1034373ee9181ff94c03877b721e36
Julee Kim <summerbreeze0323@gmail.com>
2021-03-22 11:47:16 +0900

    Create Account E2E part Two

--- cypress/integration/auth/create-account.ts
+++ cypress/integration/auth/create-account.ts
@@ -17,13 +17,32 @@ describe("Create Account", () => {
   });
 
   it("should be able to create account and login", () => {
+    user.intercept("http://localhost:4000/graphql", (req) => {
+      console.log(req.body);
+      const { operationName } = req.body;
+      if (operationName && operationName === "createAccountMutation") {
+        req.reply((res) => {
+          res.send({
+            data: {
+              createAccount: {
+                ok: true,
+                error: null,
+                __typename: "CreateAccountOutput",
+              },
+            },
+          });
+        });
+      }
+    });
+
     user.visit("/create-account");
-    user.findByPlaceholderText(/email/i).type("3333@mail.com");
-    user.findByPlaceholderText(/password/i).type("real@mail.com");
+    user.findByPlaceholderText(/email/i).type("julee0323@naver.com");
+    user.findByPlaceholderText(/password/i).type("12345");
     user.findByRole("button").click();
     user.wait(1000);
-    user.findByPlaceholderText(/email/i).type("3333@mail.com");
-    user.findByPlaceholderText(/password/i).type("real@mail.com");
+    user.title().should("eq", "Login | Nuber Eats");
+    user.findByPlaceholderText(/email/i).type("julee0323@naver.com");
+    user.findByPlaceholderText(/password/i).type("12345");
     user.findByRole("button").click();
     user.window().its("localStorage.nuber-token").should("be.a", "string");
   });
