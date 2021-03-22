83ee194c7ad15942a1a5c9d3446b3b479ca8cfe4
Julee Kim <summerbreeze0323@gmail.com>
2021-03-22 17:35:10 +0900

    Create Restaurant part Two

--- src/pages/owner/add-restaurants.tsx
+++ src/pages/owner/add-restaurants.tsx
@@ -1,8 +1,9 @@
 import { gql, useMutation } from '@apollo/client';
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useForm } from 'react-hook-form';
 import { Button } from '../../components/button';
+import { FormError } from '../../components/form-error';
 import { createRestaurant, createRestaurantVariables } from '../../__generated__/createRestaurant';
 
 const CREATE_RESTAURANT_MUTATION = gql`
@@ -18,13 +19,20 @@ interface IFormProps {
   name: string;
   address: string;
   categoryName: string;
+  file: FileList;
 }
 
 export const AddRestaurant = () => {
+  const [uploading, setUploading] = useState(false);
+
+  const onCompleted = (data: createRestaurant) => {
+    const { createRestaurant: { ok } } = data;
+    if (ok) setUploading(false);
+  }
   const [createRestaurantMutation, { loading, data }] = useMutation<
     createRestaurant,
     createRestaurantVariables
-    >(CREATE_RESTAURANT_MUTATION);
+    >(CREATE_RESTAURANT_MUTATION, { onCompleted });
   
   const {
     register,
@@ -34,17 +42,44 @@ export const AddRestaurant = () => {
     handleSubmit
   } = useForm<IFormProps>({ mode: 'onChange' });
 
-  const onSubmit = () => {
-    console.log(getValues());
+  const onSubmit = async () => {
+    setUploading(true);
+
+    const { file, name, categoryName, address } = getValues();
+    const actualFile = file[0];
+    const formBody = new FormData();
+
+    formBody.append('file', actualFile);
+
+    const { url: coverImg } = await (
+      await fetch("http://localhost:4000/uploads/", {
+        method: "POST",
+        body: formBody,
+      })
+    ).json();
+
+    createRestaurantMutation({
+      variables: {
+        input: {
+          name,
+          categoryName,
+          address,
+          coverImg
+        }
+      }
+    });
   }
 
   return (
-    <div className="container">
+    <div className="container flex flex-col items-center mt-52">
       <Helmet>
         <title>Add Restaurant | Nuber Eats</title>
       </Helmet>
-      <h1>Add Restaurant</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <h4 className="font-semibold text-2xl mb-3">Add Restaurant</h4>
+      <form
+        className="grid max-w-screen-sm gap-3 mt-5 w-full mb-5"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <input
           className="input"
           type="text"
@@ -66,11 +101,22 @@ export const AddRestaurant = () => {
           placeholder="Category Name"
           ref={register({ required: "Category Name is required." })}
         />
+        <div>
+          <input
+            type="file"
+            name="file"
+            accept="image/*"
+            ref={register({ required: true })}
+          />
+        </div>
         <Button
-          loading={loading}
+          loading={uploading}
           canClick={formState.isValid}
           actionText="Create Restaurant"
         />
+        {data?.createRestaurant?.error && (
+          <FormError errorMessage={data.createRestaurant.error} />
+        )}
       </form>
     </div>
   );
