11e0c8343e472d930c7a6f6312cce04873fbcdd5
Julee Kim <summerbreeze0323@gmail.com>
2021-03-22 10:03:47 +0900

    Our First Cypress Test

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,7 +1,24 @@
-describe('First Test', () => {
-  it('should go to homepage', () => {
-    cy.visit('http://localhost:3000')
-      .title()
-      .should('eq', 'Login | Nuber Eats');
+describe('Log In', () => {
+  it('should see login page', () => {
+    cy.visit('/').title().should('eq', 'Login | Nuber Eats');
+  });
+
+  it('can fill out the form', () => {
+    cy.visit('/')
+      .findByPlaceholderText(/email/i)
+      .type('julee@naver.com')
+      .findByPlaceholderText(/password/i)
+      .type('123')
+      .findByRole('button')
+      .should('not.have.class', 'pointer-events-none')
+      // to do (can log in)
+  });
+  
+  it('can see email / password validation errors', () => {
+    cy.visit('/')
+      .findByPlaceholderText(/email/i)
+      .type('bad@email')
+      .findByRole('button')
+      .should('have.text', 'Please enter a valid email')
   });
 });
