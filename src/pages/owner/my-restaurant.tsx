948912174065c6038caaad7d05a71d7312658434
Julee Kim <summerbreeze0323@gmail.com>
2021-03-22 21:05:49 +0900

    Restaurant Dashboard part One

---
+++ src/pages/owner/my-restaurant.tsx
@@ -0,0 +1,40 @@
+import { gql, useQuery } from '@apollo/client';
+import React from 'react';
+import { useParams } from 'react-router';
+import { RESTAURANT_FRAGMENT } from '../../fragments';
+import { myRestaurant, myRestaurantVariables } from '../../__generated__/myRestaurant';
+
+const MY_RESTAURANT_QUERY = gql`
+  query myRestaurant($input: MyRestaurantInput!) {
+    myRestaurant(input: $input) {
+      ok
+      error
+      restaurant {
+        ...RestaurantParts
+      }
+    }
+  }
+  ${RESTAURANT_FRAGMENT}
+`;
+
+interface IParams {
+  id: string;
+}
+
+export const MyRestaurant = () => {
+  const { id } = useParams<IParams>();
+  const { data } = useQuery<myRestaurant, myRestaurantVariables>(
+    MY_RESTAURANT_QUERY, 
+    {
+      variables: {
+        input: {
+          id: +id
+        }
+      }
+    }
+  );
+  console.log(data)
+  return (
+    <h1>my restaurant!</h1>
+  )
+}
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -13,6 +13,7 @@ import { Restaurant } from '../pages/client/restaurant';
 import { Restaurants } from '../pages/client/restaurants';
 import { Search } from '../pages/client/search';
 import { AddRestaurant } from '../pages/owner/add-restaurants';
+import { MyRestaurant } from '../pages/owner/my-restaurant';
 import { MyRestaurants } from '../pages/owner/my-restaurants';
 import { ConfirmEmail } from '../pages/user/confirm-email';
 import { EditProfile } from '../pages/user/edit-profile';
@@ -50,6 +51,7 @@ const commonRoutes = [
 const restaurantRoutes = [
   { path: '/', component: <MyRestaurants /> },
   { path: '/add-restaurant', component: <AddRestaurant /> },
+  { path: '/restaurants/:id', component: <MyRestaurant /> },
 ]
 
 export const LoggedInRouter = () => {
