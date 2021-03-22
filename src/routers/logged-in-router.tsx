a5efc20ece73300999b3a8d51b6246cef53cf902
Julee Kim <summerbreeze0323@gmail.com>
2021-03-22 15:53:26 +0900

    Create Restaurant part One

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -12,6 +12,7 @@ import { Category } from '../pages/client/category';
 import { Restaurant } from '../pages/client/restaurant';
 import { Restaurants } from '../pages/client/restaurants';
 import { Search } from '../pages/client/search';
+import { AddRestaurant } from '../pages/owner/add-restaurants';
 import { MyRestaurants } from '../pages/owner/my-restaurants';
 import { ConfirmEmail } from '../pages/user/confirm-email';
 import { EditProfile } from '../pages/user/edit-profile';
@@ -46,7 +47,10 @@ const commonRoutes = [
   },
 ];
 
-const restaurantRoutes = [{ path: '/', component: <MyRestaurants/>}]
+const restaurantRoutes = [
+  { path: '/', component: <MyRestaurants /> },
+  { path: '/add-restaurant', component: <AddRestaurant /> },
+]
 
 export const LoggedInRouter = () => {
   const { data, loading, error } = useMe();
@@ -66,14 +70,14 @@ export const LoggedInRouter = () => {
       <Switch>        
         {data.me.role === 'Client' &&
           clientRoutes.map(route => (
-            <Route key={route.path} path={route.path}>
+            <Route key={route.path} path={route.path} exact>
               {route.component}
             </Route>
           ))
         }
         {data.me.role === 'Owner' &&
           restaurantRoutes.map(route => (         
-            <Route key={route.path} path={route.path}>
+            <Route key={route.path} path={route.path} exact>
               {route.component}
             </Route>
           ))
