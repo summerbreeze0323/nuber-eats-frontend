033942b1dcddbdc65e4fed62d1edc7d9b7d90424
Julee Kim <summerbreeze0323@gmail.com>
2021-03-23 23:11:28 +0900

    Order Component

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -12,6 +12,7 @@ import { Category } from '../pages/client/category';
 import { Restaurant } from '../pages/client/restaurant';
 import { Restaurants } from '../pages/client/restaurants';
 import { Search } from '../pages/client/search';
+import { Order } from '../pages/order';
 import { AddDish } from '../pages/owner/add-dish';
 import { AddRestaurant } from '../pages/owner/add-restaurants';
 import { MyRestaurant } from '../pages/owner/my-restaurant';
@@ -46,6 +47,10 @@ const commonRoutes = [
   {
     path: '/edit-profile',
     component: <EditProfile/>
+  },
+  {
+    path: '/orders/:id',
+    component: <Order/>
   },
 ];
 
