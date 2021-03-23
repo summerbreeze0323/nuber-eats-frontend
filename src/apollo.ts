91fbed0b0a893b3854b43cec97928d0573c9bc03
Julee Kim <summerbreeze0323@gmail.com>
2021-03-24 00:24:58 +0900

    Subscription Setup

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,26 +1,58 @@
-import { ApolloClient, createHttpLink, InMemoryCache, makeVar } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { LOCALSTORAGE_TOKEN } from './constants';
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+  split,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { LOCALSTORAGE_TOKEN } from "./constants";
 
-const token = localStorage.getItem(LOCALSTORAGE_TOKEN)
+const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
 export const isLoggedInVal = makeVar(Boolean(token));
 export const authTokenVal = makeVar(token);
 
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:4000/graphql",
+  options: {
+    reconnect: true,
+    connectionParams: {
+      "x-jwt": authTokenVal() || "",
+    },
+  },
+});
+
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql'
+  uri: "http://localhost:4000/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      'x-jwt': authTokenVal() || ''
-    }
-  }
+      "x-jwt": authTokenVal() || "",
+    },
+  };
 });
 
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    // TRUE return => wsLink
+    // FALSE return => authLink(httpLink)
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: splitLink,
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
@@ -28,15 +60,15 @@ export const client = new ApolloClient({
           isLoggedIn: {
             read() {
               return isLoggedInVal();
-            }
+            },
           },
           token: {
             read() {
               return authTokenVal();
-            }
-          }
-        }
-      }
-    }
-  })
+            },
+          },
+        },
+      },
+    },
+  }),
 });
