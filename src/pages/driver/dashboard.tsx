322d2330bd764484a38ef13df28b1d60f99a8b73
Julee Kim <summerbreeze0323@gmail.com>
2021-03-25 11:06:08 +0900

    Cooked Order Subscription

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,5 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import GoogleMapReact from 'google-map-react';
+import { FULL_ORDER_FRAGMENT } from '../../fragments';
+import { gql, useSubscription } from '@apollo/client';
+import { cookedOrders } from '../../__generated__/cookedOrders';
+import { Link } from 'react-router-dom';
+
+const COOKED_ORDERS_SUBSCRIPTION = gql`
+  subscription cookedOrders {
+    cookedOrders {
+      ...FullOrderParts
+    }
+  }
+  ${FULL_ORDER_FRAGMENT}
+`;
 
 interface ICoords {
   lat: number;
@@ -53,15 +66,15 @@ export const Dashboard = () => {
   };
 
   // Google API를 이용해서 경로 만들기
-  const onGetRouteClick = () => {
+  const makeRoute = () => {
     if (map) {
       const directionsService = new google.maps.DirectionsService();
       const directionsRenderer = new google.maps.DirectionsRenderer({
-        // polylineOptions: {
-        //   strokeColor: '#000',
-        //   strokeOpacity: 1,
-        //   strokeWeight: 5
-        // }
+        polylineOptions: {
+          strokeColor: '#000',
+          strokeOpacity: 1,
+          strokeWeight: 5
+        }
       });
       directionsRenderer.setMap(map);
       directionsService.route(
@@ -85,7 +98,16 @@ export const Dashboard = () => {
         }
       )
     }
-  }
+  };
+
+  const { data: cookedOrdersData } = useSubscription<cookedOrders>(
+    COOKED_ORDERS_SUBSCRIPTION
+  );
+  useEffect(() => {
+    if (cookedOrdersData?.cookedOrders.id) {
+      makeRoute();
+    }
+  }, [cookedOrdersData]);
 
   return (
     <div>
@@ -99,7 +121,29 @@ export const Dashboard = () => {
           bootstrapURLKeys={{ key: 'AIzaSyB_hBJq6YemWYKFq_2KqoVLzCjgW6Da7WU' }}
         ></GoogleMapReact>
       </div>
-      <button onClick={onGetRouteClick}>Get route</button>
+      <div className="max-w-screen-sm mx-auto bg-white relative -top-10 shadow-lg py-8 px-5">
+        {cookedOrdersData?.cookedOrders.restaurant ? (
+          <>
+            <h1 className="text-center text-3xl font-medium">
+              New Cooked Order
+            </h1>
+            <h1 className="text-center my-3 text-3xl font-medium">
+              Pick it up soon @{' '}
+              {cookedOrdersData?.cookedOrders.restaurant?.name}
+            </h1>
+            <Link
+              to={`/orders/${cookedOrdersData?.cookedOrders.id}`}
+              className="btn w-full block text-center mt-5"
+            >
+              Accept Challenge &rarr;
+            </Link>
+          </>
+        ) : (
+          <h1 className="text-center text-3xl font-medium">
+            No orders yet...
+          </h1>
+        )}
+      </div>
     </div>
   );
 }
